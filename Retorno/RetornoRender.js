93df8083fda59d56d18166c5009d7a4f9aa55109
Geovanny García <wgarciamunoz@gmail.com>
2023-05-15 16:12:48 -0500

    User Control manejo de propiedades

---
+++ Retorno/RetornoRender.js
@@ -0,0 +1,22 @@
+function Retorno($)
+{
+	this.Simples;
+	this.Atributo;
+
+	// Databinding for property Atributo
+	this.SetAtributo = function(data)
+	{
+		this.Atributo = data;
+	}
+
+	// Databinding for property Atributo
+	this.GetAtributo = function()
+	{
+		return this.Atributo;
+	}
+
+	this.show = function()
+	{
+		
+	}
+}
---
+++ UCControl/UCControlRender.js
@@ -0,0 +1,13 @@
+function UCControl($)
+{
+	this.show = function()
+	{
+		var x = this.Propiedad;
+		var html = '';
+		html += '<h1>Hello world!</h1>';
+		Console.log("x" + x);
+		this.setHtml(html);
+	}
+
+
+}
