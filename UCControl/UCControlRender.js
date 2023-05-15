1dfff148bc26830a600160f471481d7b4e4435ef
Geovanny García <wgarciamunoz@gmail.com>
2023-05-15 17:24:03 -0500

    se agregan propiedades y se agrupan propiedades en nuevas categorías definidas con <Group>

--- UCControl/UCControlRender.js
+++ UCControl/UCControlRender.js
@@ -2,11 +2,10 @@ function UCControl($)
 {
 	this.show = function()
 	{
-		var x = this.Propiedad;
-		var html = '';
-		html += '<h1>Hello world!</h1>';
-		Console.log("x" + x);
-		this.setHtml(html);
+		var html = '<span>';
+		html    +=  this.Propiedad;
+		html    += '</span>';
+		this.setHtml(html); 
 	}
 
 
